56aae30dd9c9225c67e2f479882cda303df84830
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-07 21:42:26 +0100

    more changes made'

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const newItemForm = document.querySelector('#new-item-form');
-  newItemForm.addEventListener('submit', handleNewItemFormSubmit);
+  const form = document.querySelector('#new-item-form');
+  form.addEventListener('submit', handleNewItemFormSubmit);
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
-
 })
 
-const handleNewItemFormSubmit = function (event) {
+const handleNewItemFormSubmit = () => {
   event.preventDefault()
   console.log(event);
 
@@ -15,26 +14,26 @@ const handleNewItemFormSubmit = function (event) {
   const animalList = document.querySelector('#animal-list');
   animalList.appendChild(animalListItem);
 
+
   event.target.reset();
 }
 
-const createAnimalListItem = function() {
+const createAnimalListItem = function(form) {
   const newAnimal = document.createElement('li');
 
-  const name = document.createElement('h2');
-  name.textContent = event.target.name.value;
+  const name = document.createElement('h2')
+  name.textContent = `Animal: ${form.name.value}`;
   newAnimal.appendChild(name);
 
-  const species = document.createElement('h3');
-  species.textContent = event.target.species.value;
+  const species = document.createElement('h3')
+  species.textContent = `Species: ${form.species.value}`;
   newAnimal.appendChild(species);
 
-  const category = document.createElement('p');
-  category.textContent = event.target.continent.value;
-  newAnimal.append(category);
+  const continent = document.createElement('p')
+  continent.textContent = `Continent: ${form.continent.value}`;
+  newAnimal.appendChild(continent);
 
   return newAnimal;
-
 }
 
 const handleDeleteAllClick = function (event) {
