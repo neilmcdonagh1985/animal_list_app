a58b2b103bc1e07525f6b882de193e8637192ad6
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-07 12:19:11 +0100

    some css done'

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const deleteAllButton = document.querySelector('#delete-all');
   deleteAllButton.addEventListener('click', handleDeleteAllClick);
+
 })
 
 const handleNewItemFormSubmit = function (event) {
@@ -15,28 +16,28 @@ const handleNewItemFormSubmit = function (event) {
   animalList.appendChild(animalListItem);
 
   event.target.reset();
-  }
+}
 
-const createAnimalListItem = function(form) {
+const createAnimalListItem = function() {
   const newAnimal = document.createElement('li');
 
   const name = document.createElement('h2');
-  name.textContent = form.name.value;
+  name.textContent = event.target.name.value;
   newAnimal.appendChild(name);
 
   const species = document.createElement('h3');
-  species.textContent = form.species.value;
+  species.textContent = event.target.species.value;
   newAnimal.appendChild(species);
 
   const category = document.createElement('p');
-  category.textContent = form.category.value;
+  category.textContent = event.target.continent.value;
   newAnimal.append(category);
 
   return newAnimal;
 
-  }
+}
 
-  const handleDeleteAllClick = function (event) {
-    const animalList = document.querySelector('#animal-list');
-    animalList.innerHTML = '';
-  }
+const handleDeleteAllClick = function (event) {
+  const animalList = document.querySelector('#animal-list');
+  animalList.innerHTML = '';
+}
