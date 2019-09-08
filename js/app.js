bcfafe97b4a717ff37bbb4ee367c27ebceb44cca
Neil McDonagh <neiljamesmcdonagh@gmail.com>
2019-09-08 21:22:34 +0100

    mvp re done from scratch

--- js/app.js
+++ js/app.js
@@ -1,43 +1,38 @@
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () =>{
   const form = document.querySelector('#new-item-form');
-  form.addEventListener('submit', handleNewItemFormSubmit);
+
+  const deleteAll = function () {
+    const animalList = document.querySelector('#animal-list');
+    animalList.innerHTML = '';
+  }
 
   const deleteAllButton = document.querySelector('#delete-all');
-  deleteAllButton.addEventListener('click', handleDeleteAllClick);
+  deleteAllButton.addEventListener('click', deleteAll);
+
+  form.addEventListener('submit', function() {
+    event.preventDefault()
+    console.log(event);
+
+    const newList = document.querySelector('#animal-list');
+
+      const newListItem = document.createElement('li');
+
+      const name = document.createElement('h2');
+      name.textContent = `Animal Name: ${event.target.name.value}`;
+      newListItem.appendChild(name);
+
+      const species = document.createElement('h3');
+      species.textContent = `Species: ${event.target.species.value}`;
+      newListItem.appendChild(species);
+
+      const continent = document.createElement('p');
+      continent.textContent = `Continent: ${event.target.continent.value}`;
+      newListItem.appendChild(continent);
+
+      newList.appendChild(newListItem);
+      event.target.reset();
+
+      return newListItem;    
+  })
 
 })
-
-const createAnimalListItem = function() {
-  const newAnimal = document.createElement('li');
-
-  const name = document.createElement('h2')
-  name.textContent = `Animal: ${event.target.name.value}`;
-  newAnimal.appendChild(name);
-
-  const species = document.createElement('h3')
-  species.textContent = `Species: ${event.target.species.value}`;
-  newAnimal.appendChild(species);
-
-  const continent = document.createElement('p')
-  continent.textContent = `Continent: ${event.target.continent.value}`;
-  newAnimal.appendChild(continent);
-
-  return newAnimal;
-}
-
-const handleNewItemFormSubmit = () => {
-  event.preventDefault()
-  console.log(event);
-
-  const animalListItem = createAnimalListItem(event.target);
-  const animalList = document.querySelector('#animal-list');
-  animalList.appendChild(animalListItem);
-
-  event.target.reset();
-
-
-}
-const handleDeleteAllClick = function (event) {
-  const animalList = document.querySelector('#animal-list');
-  animalList.innerHTML = '';
-}
